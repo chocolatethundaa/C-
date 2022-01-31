c82c35babb37c4849a1cdf0f2d3c34264e845f69
Andry Canel <canel2@illinois.edu>
2022-01-30 20:09:36 -0500

    was getting error and found out my folder name was the reason why code wouldnt compile

---
+++ ClassesObjects/RobotClass.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+class Robot{
+private:
+    string name;
+    string ownerName;
+    double price;
+
+public:
+    Robot(string Name, string OwnerName){
+        name = Name;
+        ownerName = OwnerName;
+    }
+
+    void getInfo(){
+        cout<< "This robot name is "<< name << " and the owner is "<< ownerName<< " ."<< endl;
+    }
+
+};
+
+int main(){
+
+ Robot Exo("Chappy", "Andry Canel");
+ Exo.getInfo();
+
+ system("pause>0"); 
+
+
+ return 0;   
+}
