dad343e4647ddee29cfd9ec88b3667c9b722e1e5
Andry Canel <canel2@illinois.edu>
2022-02-03 18:24:41 -0500

    made a fuction that prints out a character with passing a pointer by reference

--- Pointers/basicPtr.cpp
+++ Pointers/basicPtr.cpp
@@ -4,9 +4,20 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+void printChar (void* cPtr){
 
+    cout << *((char*)cPtr) <<endl; 
+}
 int main(){
 
+/* Point of pointer is for example:
+pass values by reference to a function 
+to return multiple values from a fucntion
+in combination with arrays
+with dynamic memory allocation
+smart pointers
+access an object of a derived class through the base class
+*/
 
 int n = 10;
 
@@ -17,8 +28,12 @@ cout << *ptr <<endl;//prints the value of n which is 10 rn
 cout << &ptr <<endl;//prints the address of ptr
 
 cout << &n << endl;//prints the address of n
-*ptr = 20; // can actually change the value at n be referencing it through ptr
+*ptr = 20; // can actually change the value at n be dereferencing it through ptr
 cout << n << endl;
+
+char bruh = 'v';
+char* v = &bruh;
+printChar(&bruh);
 
 system("pause>0");
 
