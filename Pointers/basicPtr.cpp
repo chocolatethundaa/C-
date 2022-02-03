8dec1622a542447981bf06363b442eb55bd9f1ed
Andry Canel <canel2@illinois.edu>
2022-02-03 18:10:59 -0500

    dont know where i left off yesterday so i am going to commit

--- Pointers/basicPtr.cpp
+++ Pointers/basicPtr.cpp
@@ -10,7 +10,15 @@ int main(){
 
 int n = 10;
 
-cout << &n << endl;
+int* ptr = &n; // basically holds the adress of a value
+
+cout << ptr <<endl;//prints the address of n
+cout << *ptr <<endl;//prints the value of n which is 10 rn
+cout << &ptr <<endl;//prints the address of ptr
+
+cout << &n << endl;//prints the address of n
+*ptr = 20; // can actually change the value at n be referencing it through ptr
+cout << n << endl;
 
 system("pause>0");
 
